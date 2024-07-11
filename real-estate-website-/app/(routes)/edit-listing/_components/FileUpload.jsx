6b637d0ded533bf3e14c5ce3a9f864ab41888c1f
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-11 14:43:33 +0530

    Updated - Given the functionality where the image upload form will now accept - jpg, jpeg, png, gif, avif types of images.

--- real-estate-website-/app/(routes)/edit-listing/_components/FileUpload.jsx
+++ real-estate-website-/app/(routes)/edit-listing/_components/FileUpload.jsx
@@ -26,7 +26,14 @@ function FileUpload(){
                     <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
                     <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                 </div>
-                <input id="dropzone-file" type="file" multiple onChange={handleFileUpload} className="hidden" />
+                <input 
+                    id="dropzone-file" 
+                    type="file" 
+                    multiple 
+                    onChange={handleFileUpload} 
+                    className="hidden" 
+                    accept='image/jpg, image/jpeg, image/png, image/gif, image/avif'
+                />
             </label>
         </div> 
 
