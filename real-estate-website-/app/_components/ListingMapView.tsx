8a8be04fdcf9c646b173eadaa55785c98152ab9a
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-14 02:04:11 +0530

    Successfully extracted all the listing from the supabase and logged the data and the data whole type is true that listing will only be displayed and also gave the function inside an useEffect hook to check it as soon as the website opens.

--- real-estate-website-/app/_components/ListingMapView.tsx
+++ real-estate-website-/app/_components/ListingMapView.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Listing from './Listing'
+import { supabase } from '@/supabase/client'
 
-function ListingMapView() {
+function ListingMapView({type}:any) {
 
-  
+  useEffect(() => {
+    getLatestListing();
+  },[])
+
+  const getLatestListing = async() => {
+    const {data, error} = await supabase
+    .from('listing')
+    .select(`*, listingImages(
+      imgUrl,
+      listing_id
+    )`)
+    .eq('active', type)
+    .eq('type', type)
+
+    if(data) {
+      console.log(data);
+      
+    }
+  }
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2'>
