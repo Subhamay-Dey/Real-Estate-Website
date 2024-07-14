fc1b4343c92d7b49b73560ef1d2a6f401c0a9439
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-14 21:21:45 +0530

    Rendered the data of the no of bedroom, bathroom and the plot area and showed it under their database's image.

--- real-estate-website-/app/_components/Listing.tsx
+++ real-estate-website-/app/_components/Listing.tsx
@@ -1,13 +1,14 @@
-import { MapPin } from 'lucide-react'
+import { Bath, BedDouble, MapPin, Ruler } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
 function Listing({listing}:any) {
+
   return (
     <div>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
         {listing.map((item: any, index: any) => (
-          <div>
+          <div className='p-3 hover:border-primary'>
             <Image src={item.listingImages[0].imgUrl}
               width={800}
               height={150}
@@ -20,6 +21,22 @@ function Listing({listing}:any) {
                 <MapPin className='h-4 w-4'/>
               {item.address}</h2>
             </div>
+
+            <div className='flex gap-2 mt-2 justify-between'>
+                <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                  <BedDouble className='h-4 w-4'/>
+                  {item?.bedroom}
+                </h2>
+                <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                  <Bath className='h-4 w-4'/>
+                  {item?.bathroom}
+                </h2>
+                <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                  <Ruler className='h-4 w-4'/>
+                  {item?.area}
+                </h2>
+            </div>
+
           </div>
         ))}
       </div>
