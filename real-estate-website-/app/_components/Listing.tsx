084542ca0ba55c63178154c84db9a21b46555eee
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-14 19:40:41 +0530

    Mapped the listing Images in the listing component and the database images are being successfully displayed and also gave their prices from the database.

--- real-estate-website-/app/_components/Listing.tsx
+++ real-estate-website-/app/_components/Listing.tsx
@@ -1,9 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Listing({listing}:any) {
   return (
     <div>
-      
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+        {listing.map((item: any, index: any) => (
+          <div>
+            <Image src={item.listingImages[0].imgUrl}
+              width={800}
+              height={250}
+              className='rounded-lg h-[250px]'
+              alt='listingImage'
+            />
+            <div>
+              <h2 className='font-bold text-xl'>${item.price}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
