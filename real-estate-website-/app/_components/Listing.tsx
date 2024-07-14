5a60da00f95caa472eeec51dbe38c574197d3173
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-14 23:40:26 +0530

    Liting updated - finally have the skeleton effect untill the listing are loading that tine the skeleton will show.

--- real-estate-website-/app/_components/Listing.tsx
+++ real-estate-website-/app/_components/Listing.tsx
@@ -7,12 +7,12 @@ function Listing({listing}:any) {
   return (
     <div>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-        {listing.map((item: any, index: any) => (
+        {listing?.length > 0 ? listing.map((item: any, index: any) => (
           <div className='p-3 hover:border hover:border-primary cursor-pointer rounded-lg'>
             <Image src={item.listingImages[0].imgUrl}
               width={800}
               height={150}
-              className='rounded-lg object-cover h-[220px]'
+              className='rounded-lg object-cover h-[200px]'
               alt='listingImage'
             />
             <div className='flex mt-2 flex-col gap-2'>
@@ -20,25 +20,32 @@ function Listing({listing}:any) {
               <h2 className='flex gap-2 text-sm text-gray-400'>
                 <MapPin className='h-4 w-4'/>
               {item.address}</h2>
-            </div>
+            
 
-            <div className='flex gap-2 mt-2 justify-between'>
-                <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
-                  <BedDouble className='h-4 w-4'/>
-                  {item?.bedroom}
-                </h2>
-                <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
-                  <Bath className='h-4 w-4'/>
-                  {item?.bathroom}
-                </h2>
-                <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
-                  <Ruler className='h-4 w-4'/>
-                  {item?.area}
-                </h2>
+              <div className='flex gap-2 mt-2 justify-between'>
+                  <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                    <BedDouble className='h-4 w-4'/>
+                    {item?.bedroom}
+                  </h2>
+                  <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                    <Bath className='h-4 w-4'/>
+                    {item?.bathroom}
+                  </h2>
+                  <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                    <Ruler className='h-4 w-4'/>
+                    {item?.area}
+                  </h2>
+              </div>
             </div>
 
           </div>
-        ))}
+        ))
+      :[1,2,3,4,5,6,7,8].map((item, index)=>(
+          <div key={index} className='h-[340px] w-full bg-slate-200 animate-pulse rounded-lg'>
+            
+          </div>
+      ))
+      }
       </div>
     </div>
   )
