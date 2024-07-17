179bbe7ba305af2ce70790193380aa0de3e013ca
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-17 23:59:42 +0530

    used the select component of shadcn where I will be providing the droopdown of the bedroom counts for the user inputs.

--- real-estate-website-/app/_components/FilterSection.tsx
+++ real-estate-website-/app/_components/FilterSection.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+  
 
 function FilterSection() {
   return (
-    <div>FilterSection</div>
+    <div>
+        <Select>
+        <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Bed" />
+        </SelectTrigger>
+        <SelectContent>
+            <SelectItem value="light">2+</SelectItem>
+            <SelectItem value="dark">3+</SelectItem>
+            <SelectItem value="system">4+</SelectItem>
+        </SelectContent>
+        </Select>
+
+    </div>
   )
 }
 
