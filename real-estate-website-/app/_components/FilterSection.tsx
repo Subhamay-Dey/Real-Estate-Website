faa49683d9c5ceb31007663e4c1b57fa274bad65
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-18 00:56:47 +0530

    completed the bedroom component.

--- real-estate-website-/app/_components/FilterSection.tsx
+++ real-estate-website-/app/_components/FilterSection.tsx
@@ -6,22 +6,30 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select"
-  
+import { BedDouble } from 'lucide-react';
 
 function FilterSection() {
   return (
-    <div>
+    <div className='px-3 py-4 grid grid-cols-2 md:flex gap-2'>
         <Select>
         <SelectTrigger className="w-[180px]">
             <SelectValue placeholder="Bed" />
         </SelectTrigger>
         <SelectContent>
-            <SelectItem value="light">2+</SelectItem>
-            <SelectItem value="dark">3+</SelectItem>
-            <SelectItem value="system">4+</SelectItem>
+            <SelectItem value="2">
+                <h2 className='flex gap-2'><BedDouble className='h-5 w-5 text-primary'/>2+</h2>
+            </SelectItem>
+            <SelectItem value="3">
+                <h2 className='flex gap-2'><BedDouble className='h-5 w-5 text-primary'/>3+</h2>
+            </SelectItem>
+            <SelectItem value="4">
+                <h2 className='flex gap-2'><BedDouble className='h-5 w-5 text-primary'/>4+</h2>
+            </SelectItem>
+            <SelectItem value="5">
+                <h2 className='flex gap-2'><BedDouble className='h-5 w-5 text-primary'/>5+</h2>
+            </SelectItem>
         </SelectContent>
         </Select>
-
     </div>
   )
 }
