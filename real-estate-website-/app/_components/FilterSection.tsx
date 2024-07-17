43173b79b6cb1acb0cf1b47b981dd4ed4e94a0d2
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-18 01:04:16 +0530

    completed the bathroom component.

--- real-estate-website-/app/_components/FilterSection.tsx
+++ real-estate-website-/app/_components/FilterSection.tsx
@@ -6,7 +6,7 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select"
-import { BedDouble } from 'lucide-react';
+import { Bath, BedDouble } from 'lucide-react';
 
 function FilterSection() {
   return (
@@ -27,6 +27,25 @@ function FilterSection() {
             </SelectItem>
             <SelectItem value="5">
                 <h2 className='flex gap-2'><BedDouble className='h-5 w-5 text-primary'/>5+</h2>
+            </SelectItem>
+        </SelectContent>
+        </Select>
+        <Select>
+        <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Bath" />
+        </SelectTrigger>
+        <SelectContent>
+        <SelectItem value="1">
+                <h2 className='flex gap-2'><Bath className='h-5 w-5 text-primary'/>1+</h2>
+            </SelectItem>
+            <SelectItem value="2">
+                <h2 className='flex gap-2'><Bath className='h-5 w-5 text-primary'/>2+</h2>
+            </SelectItem>
+            <SelectItem value="3">
+                <h2 className='flex gap-2'><Bath className='h-5 w-5 text-primary'/>3+</h2>
+            </SelectItem>
+            <SelectItem value="4">
+                <h2 className='flex gap-2'><Bath className='h-5 w-5 text-primary'/>4+</h2>
             </SelectItem>
         </SelectContent>
         </Select>
