b4925999aa77c9264c6d82bd4245f7aadcb91387
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-19 14:28:52 +0530

    finally the default google map pointer is showing on the exact address of the user's input search.

--- real-estate-website-/app/_components/MarkerItem.tsx
+++ real-estate-website-/app/_components/MarkerItem.tsx
@@ -1,9 +1,14 @@
+import { MarkerF } from '@react-google-maps/api'
 import React from 'react'
 
 function MarkerItem({item}: any) {
   return (
     <div>
-      
+      <MarkerF
+        position={item.coordinates}
+      >
+
+      </MarkerF>
     </div>
   )
 }
