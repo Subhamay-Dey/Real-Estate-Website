40df2125fd381c28ca7fb52a44d4f793a0d6937c
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-19 23:58:48 +0530

    Updated - the view-listing route will open on every listing house when the user clicks on it and the specific id view-listing route will open with the listing of the id details.

--- real-estate-website-/app/_components/Listing.tsx
+++ real-estate-website-/app/_components/Listing.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import GoogleAddressSearch from './GoogleAddressSearch'
 import { Button } from '@/components/ui/button'
 import FilterSection from './FilterSection'
+import Link from 'next/link'
 
 function Listing({listing , handleSearchClick, searchedAddress, setBedCount, setBathCount, setParkingCount, setHomeType, setCoordinates}:any) {
 
@@ -36,36 +37,38 @@ function Listing({listing , handleSearchClick, searchedAddress, setBedCount, set
       </div>}
       <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
         {listing?.length > 0 ? listing.map((item: any, index: any) => (
-          <div className='p-3 hover:border hover:border-primary cursor-pointer rounded-lg'>
-            <Image src={item.listingImages[0].imgUrl}
-              width={800}
-              height={150}
-              className='rounded-lg object-cover h-[200px]'
-              alt='listingImage'
-            />
-            <div className='flex mt-2 flex-col gap-2'>
-              <h2 className='font-bold text-xl'>${item.price}</h2>
-              <h2 className='flex gap-2 text-sm text-gray-400'>
-                <MapPin className='h-4 w-4'/>
-              {item.address}</h2>
-            
+          <Link href={'/view-listing/'+item.id}>
+            <div className='p-3 hover:border hover:border-primary cursor-pointer rounded-lg'>
+              <Image src={item.listingImages[0].imgUrl}
+                width={800}
+                height={150}
+                className='rounded-lg object-cover h-[200px]'
+                alt='listingImage'
+              />
+              <div className='flex mt-2 flex-col gap-2'>
+                <h2 className='font-bold text-xl'>${item.price}</h2>
+                <h2 className='flex gap-2 text-sm text-gray-400'>
+                  <MapPin className='h-4 w-4'/>
+                {item.address}</h2>
+              
 
-              <div className='flex gap-2 mt-2 justify-between'>
-                  <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
-                    <BedDouble className='h-4 w-4'/>
-                    {item?.bedroom}
-                  </h2>
-                  <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
-                    <Bath className='h-4 w-4'/>
-                    {item?.bathroom}
-                  </h2>
-                  <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
-                    <Ruler className='h-4 w-4'/>
-                    {item?.area}
-                  </h2>
+                <div className='flex gap-2 mt-2 justify-between'>
+                    <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                      <BedDouble className='h-4 w-4'/>
+                      {item?.bedroom}
+                    </h2>
+                    <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                      <Bath className='h-4 w-4'/>
+                      {item?.bathroom}
+                    </h2>
+                    <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                      <Ruler className='h-4 w-4'/>
+                      {item?.area}
+                    </h2>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))
       :[1,2,3,4,5,6,7,8].map((item, index)=>(
           <div key={index} className='h-[340px] w-full bg-slate-200 animate-pulse rounded-lg'>
