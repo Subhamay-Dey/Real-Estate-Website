3cf669b06214caa3356ff78b9327f435e5ab72b1
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-20 23:59:40 +0530

    Imp update - Listing section of the profile section of the user is successfully completed where the user's listing will only be showed and also with their type - published or draft.

---
+++ real-estate-website-/app/(routes)/user/[[...rest]]/_components/UserListing.tsx
@@ -0,0 +1,73 @@
+import { supabase } from '@/supabase/client'
+import { useUser } from '@clerk/nextjs'
+import { Bath, BedDouble, MapPin, Ruler } from 'lucide-react';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react'
+
+function UserListing() {
+
+    const {user} = useUser();
+
+    const [listing, setListing] = useState();
+
+    useEffect(() => {
+        user && GetUserListing();
+    },[user])
+
+    const GetUserListing = async() => {
+
+        const {data, error} = await supabase
+        .from('listing')
+        .select('*, listingImages(imgUrl, listing_id)')
+        .eq('createdBy',user?.primaryEmailAddress?.emailAddress)
+
+        setListing(data);
+        console.log(data);
+        
+    }
+
+  return (
+    <div>
+        <h2 className='font-bold text-2xl'>Manage your listing</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2'>
+            {
+            listing && listing.map((item: any, index: any)=> (
+            <div className='p-3 hover:border hover:border-primary cursor-pointer rounded-lg'>
+                <h2 className='bg-primary text-white w-[90px] flex justify-center items-center rounded-t text-md'>{item.active ? "Published" : "Draft"}</h2>
+              <Image src={item?.listingImages[0]?.imgUrl}
+                width={800}
+                height={150}
+                className='rounded-lg object-cover h-[200px]'
+                alt='listingImage'
+              />
+              <div className='flex mt-2 flex-col gap-2'>
+                <h2 className='font-bold text-xl'>${item.price}</h2>
+                <h2 className='flex gap-2 text-sm text-gray-400'>
+                  <MapPin className='h-4 w-4'/>
+                {item.address}</h2>
+              
+
+                <div className='flex gap-2 mt-2 justify-between'>
+                    <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                      <BedDouble className='h-4 w-4'/>
+                      {item?.bedroom}
+                    </h2>
+                    <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                      <Bath className='h-4 w-4'/>
+                      {item?.bathroom}
+                    </h2>
+                    <h2 className='flex gap-2 text-sm bg-slate-200 rounded-md p-2 w-full text-gray-500 justify-center items-center'>
+                      <Ruler className='h-4 w-4'/>
+                      {item?.area}
+                    </h2>
+                </div>
+              </div>
+            </div>
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default UserListing
--- real-estate-website-/app/(routes)/user/[[...rest]]/page.tsx
+++ real-estate-website-/app/(routes)/user/[[...rest]]/page.tsx
@@ -1,12 +1,22 @@
-
-import { UserProfile } from '@clerk/nextjs'
+"use client"
+import { UserButton, UserProfile } from '@clerk/nextjs'
+import { Building2 } from 'lucide-react'
 import React from 'react'
+import UserListing from './_components/UserListing'
 
 function page() {
   return (
-    <div className='my-4 md:px-10 lg:px-32'>
+    <div className='my-4 md:px-10 lg:px-32 w-full'>
         <h2 className='font-bold text-2xl mb-4'>Profile</h2>
-        <UserProfile routing="hash"/>
+        <UserProfile routing="hash">
+          <UserButton.UserProfilePage
+            label='My Listing'
+            labelIcon={<Building2 className='h-5 w-5'/>}
+            url='my-listing'
+          >
+            <UserListing/>
+          </UserButton.UserProfilePage>
+        </UserProfile>
     </div>
   )
 }
