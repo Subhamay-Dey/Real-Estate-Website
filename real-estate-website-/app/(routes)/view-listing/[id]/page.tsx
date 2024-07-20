bcc20af02be621101cc43c8c24ffd25871b78eb6
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-20 08:57:11 +0530

    Extracted the full details of the particular params id from the supabase and gave the function in an useEffect hook to get the data as aoon as the user redirects to the view-listing/[id] route.

--- real-estate-website-/app/(routes)/view-listing/[id]/page.tsx
+++ real-estate-website-/app/(routes)/view-listing/[id]/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { supabase } from '@/supabase/client'
+import React, { useEffect } from 'react'
 
-function ViewListing() {
+function ViewListing({params}:any) {
+
+  useEffect(() => {
+    GetListingDetail();
+  },[])
+
+  const GetListingDetail = async() => {
+    const {data, error} = await supabase
+    .from('listing')
+    .select('*, listingImages(imgUrl, listing_id)')
+    .eq('id', params.id)
+
+    if(data) {
+      console.log(data);
+    }
+    else{
+      console.log(error.message);
+    }
+  }
+
   return (
-    <div>ViewListing</div>
+    <div>
+
+    </div>
   )
 }
 
